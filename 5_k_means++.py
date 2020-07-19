6ca657d3b4096480a40fe379e32130ef74bfea49
Coding Capricorn <thecodingcapricorn@gmail.com>
2020-07-19 10:09:22 +0530

    Update 5_k_means++.py

--- 5_k_means++.py
+++ 5_k_means++.py
@@ -11,7 +11,7 @@
 
 1. It is sensitive to the initialization of the centroids or the mean points. So, if a centroid is initialized to be a “far-off” point, it might just end up with no points associated with it, and at the same time, more than one cluster might end up linked with a single centroid.
 
-2. Similarly, more than 
+2. Simi
 
  
 
