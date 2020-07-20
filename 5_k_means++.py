944a3873bbd0b38cb5257f3463aab9ccd5689141
Coding Capricorn <thecodingcapricorn@gmail.com>
2020-07-20 09:58:27 +0530

    Update 5_k_means++.py

--- 5_k_means++.py
+++ 5_k_means++.py
@@ -9,7 +9,7 @@
 
 ###Disadvantage of the K-means algorithm ::::
 
-1. It is sensitive to the initialization of the centroids or the mean points. So, if a centroid is
+1. It is sensitive to the initialization of the centroids or the mean points. So
 
  
 
